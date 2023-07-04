671d16af71f0da89d5521a3a6045e4873987f5ef
doan.nguyenngoc <ngocdoannguyen950@gmail.com>
2023-07-04 07:29:22 +0700

    update

---
+++ server/routes/record.mjs
@@ -0,0 +1,69 @@
+import express from "express";
+// import db from "../db/conn.mjs";
+import pkg from 'mongoose';
+const { ObjectId, connection } = pkg;
+
+
+const router = express.Router();
+
+//          Query all records
+// router.get("/", async (req, res) => {
+//     let collection = await db.collection("records");
+//     let results = await collection.find({}).toArray();
+//     res.send(results).status(200);
+// });
+
+// //          Query record by id
+// router.get("/:id", async (req, res) => {
+//     let collection = await db.collection("records");
+//     let query = {_id: new ObjectId(req.params.id)};
+//     let result = await collection.findOne(query);
+
+//     if ( !result) {
+//         res.send("Not found").status(404);
+//     } else {
+//         res.send(result).status(200);
+//     }
+// });
+
+// //          Create a new record
+// router.post("/", async(req, res) => {
+//     let newDocument = {
+//         name: req.body.name,
+//         position: req.body.position,
+//         level: req.body.level
+//     };
+
+//     let collection = await db.collection("records");
+//     let result = await collection.insertOne(newDocument);
+//     res.send(result).status(204);
+// });
+
+// //          Update a record
+// router.patch("/:id", async (req, res) => {
+//     const query = {_id: new ObjectId(req.params.id)};
+//     const updates = {
+//         $set: {
+//             name: req.body.name,
+//             position: req.body.position,
+//             level: req.body.level
+//         }
+//     };
+
+//     let collection = await db.collection("records");
+//     let result = await collection.updateOne(query, updates);
+//     res.send(result).status(200);
+// });
+
+// //          Delete a record by id
+// router.delete("/:id", async (req, res) => {
+//     const query = {_id: new ObjectId(req.params.id)};
+
+//     const collection = await db.collection("records");
+//     let result = connection.deleteOne(query);
+
+//     res.send(result).status(200);
+// });
+
+
+export default router;
